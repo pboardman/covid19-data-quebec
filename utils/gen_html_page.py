d55ce0640b79700ac77ad34496f3c59cfb843a14
pboardman <pascalboardman@gmail.com>
2020-04-04 18:22:27 -0500

    Added color to data.html graph

--- utils/gen_html_page.py
+++ utils/gen_html_page.py
@@ -11,6 +11,26 @@ def chart_data_regions(regions):
     """
     Generate info to be used in the HTML page chart
     """
+    rgb_color = [
+        '198,40,40',
+        '173,20,87',
+        '106,27,154',
+        '69,39,160',
+        '40,53,147',
+        '21,101,192',
+        '2,119,189',
+        '0,131,143',
+        '0,105,92',
+        '46,125,50',
+        '85,139,47',
+        '158,157,36',
+        '158,157,36',
+        '249,168,37',
+        '255,143,0',
+        '239,108,0',
+        '216,67,21',
+        '78,52,46'
+    ]
 
     chart_data = {'dates': [], 'regions': {}}
     for region, date, new_case, total_case in regions:
@@ -20,7 +40,7 @@ def chart_data_regions(regions):
 
         # Add region data for this date
         if region not in chart_data['regions']:
-            chart_data['regions'][region] = {'label': region, 'data': []}
+            chart_data['regions'][region] = {'label': region, 'data': [], 'color': rgb_color.pop(0)}
 
         chart_data['regions'][region]['data'].append(total_case)
 
